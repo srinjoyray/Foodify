44211855928402a5a8778d5effe98982b1065139
Srinjoy Ray <srinjoyray@yahoo.in>
2022-03-26 15:16:21 +0530

    deploy

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser : true, useUnifiedT
     .catch(()=>console.log(error.message))
 
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+if(process.env.NODE_ENV==='production')
+{
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
